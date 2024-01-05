93011622f87a0fdffafac426a7685e6603034010
George Aziz <123029529+GeorgeAyy@users.noreply.github.com>
2024-01-05 19:58:00 +0200

    added web sockets to the leaderboard so that it updates real time

--- controllers/mainpage.controller.js
+++ controllers/mainpage.controller.js
@@ -1,42 +1,45 @@
- const mainpageController = [];
+const mainpageController = [];
 const User = require("../models/user.model");
 const PushupEntry = require("../models/pushupentry.model");
+const WebSocket = require('ws');
+// WebSocket broadcast function
+function broadcast(data, wss) {
+  for (const client of wss.clients) {
+    if (client.readyState === WebSocket.OPEN) {
+      client.send(data);
+    }
+  }
+}
 
 mainpageController.getMainpage = async (req, res) => {
-        // Redirect to login if the user is not authenticated
   if (!req.session.user) {
-    
     res.redirect("/login");
     return;
   }
 
   try {
-    
-    // Use Mongoose to find all users
     const users = await User.find();
     
-
-    // Create a leaderboard by summing up push-up entries for each user
     const leaderboard = users.map(async (user) => {
-      const totalPushups = await PushupEntry.find({ user: user._id }) // Assuming user._id is the reference to the user in the PushupEntry model
-        .then((entries) =>
-          entries.reduce((total, entry) => total + entry.pushups, 0)
-        );
+      const totalPushups = await PushupEntry.find({ user: user._id })
+        .then((entries) => entries.reduce((total, entry) => total + entry.pushups, 0));
 
       return { name: user.name, totalPushups };
     });
 
-    // Wait for all promises to resolve
     const results = await Promise.all(leaderboard);
-
-    // Sort the leaderboard by total push-ups in descending order
     results.sort((a, b) => b.totalPushups - a.totalPushups);
-    // Render the index page with the list of users
-    res.render("index", { user: req.session.user, users,leaderboard:results });
+
+    // Convert the leaderboard data to JSON
+    const leaderboardData = JSON.stringify(results);
+
+    // Broadcast updated leaderboard data to connected WebSocket clients
+    broadcast(leaderboardData, req.app.get('wss'));
+    res.render('index', { leaderboard: results, users, user: req.session.user });
   } catch (error) {
     console.error(error);
-    res.status(500).send("Internal Server Error");
+    res.status(500).send('Internal Server Error');
   }
-}
+};
 
 module.exports = mainpageController;
